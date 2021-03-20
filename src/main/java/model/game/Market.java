17ac9fcae93b7e4c342c8d680a5f9c6519f91e64
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-03-20 17:58:02 +0100

    Started to write Market

--- src/main/java/model/game/Market.java
+++ src/main/java/model/game/Market.java
@@ -1,11 +1,35 @@
 package it.polimi.ingsw.model.game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import it.polimi.ingsw.model.resources.Resource;
 
+/* Singleton */
 public class Market {
-	private Market instance;
+	private static Market instance;
 	private Resource[][] market_board;
 	private Resource free_marble;
+	private ArrayList<Resource> all_resources;
+
+	private Market() {
+		this.market_board = new Resource[3][4];
+		this.all_resources = new ArrayList<Resource>();
+		all_resources.add(null);
+		all_resources.add(null);
+		all_resources.add(null);
+		all_resources.add(null);
+		all_resources.add(Resource.SHIELD);
+		all_resources.add(Resource.SHIELD);
+		all_resources.add(Resource.STONE);
+		all_resources.add(Resource.STONE);
+		all_resources.add(Resource.COIN);
+		all_resources.add(Resource.COIN);
+		all_resources.add(Resource.SERVANT);
+		all_resources.add(Resource.SERVANT);
+		all_resources.add(Resource.FAITH);
+		randomizeMarket();
+	}
 
 	private void shiftRow(int index) {
 		return;
@@ -16,18 +40,33 @@ public class Market {
 	}
 
 	private void randomizeMarket() {
-		return;
+		Random random = new Random();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				Resource res = all_resources.remove(random.nextInt(all_resources.size()));
+				this.market_board[i][j] = res;
+			}
+		}
+		this.free_marble = all_resources.get(0);
 	}
 
-	public Market getIstance() {
-		return null;
+	public static Market getIstance() {
+		if (instance == null) {
+			instance = new Market();
+		}
+		return instance;
 	}
 
 	public Resource[] getColumn(int index) {
-		return null;
+		Resource[] column = {
+			market_board[0][index],
+			market_board[1][index],
+			market_board[2][index]
+		};
+		return column;
 	}
 
 	public Resource[] getRow(int index) {
-		return null;
+		return market_board[index];
 	}
 }
---
+++ src/test/java/model/game/MarketTest.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.model.game;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import it.polimi.ingsw.model.resources.Resource;
+
+public class MarketTest {
+	/**
+	 * Checks if every item is in the Market, including the free marble
+	 */
+	@Test
+	@Disabled
+	public void checkEverythingTest() {
+	}
+}
