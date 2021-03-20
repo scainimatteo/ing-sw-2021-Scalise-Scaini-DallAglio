4f4fc9cbc0193bb563b0fa3efca4b7fb99540142
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-03-20 13:53:00 +0100

    Started DevelopmentCardsOnTable

--- src/main/java/model/game/DevelopmentCardsOnTable.java
+++ src/main/java/model/game/DevelopmentCardsOnTable.java
@@ -6,11 +6,28 @@ import it.polimi.ingsw.model.card.Deck;
 public class DevelopmentCardsOnTable {
 	private Deck[][] development_card_decks;
 
+	public DevelopmentCardsOnTable(Deck[][] development_card_decks) {
+		this.development_card_decks = development_card_decks;
+	}
+
 	public DevelopmentCard[][] getTopCards() {
-		return null;
+		DevelopmentCard[][] development_card_on_top = new DevelopmentCard[3][4];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				development_card_on_top[i][j] = (DevelopmentCard)development_card_decks[i][j].getTopCard();
+			}
+		}
+		return development_card_on_top;
 	}
 
 	public void getFromDeck(DevelopmentCard chosen_card) {
-		return;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (development_card_decks[i][j].getTopCard().equals(chosen_card)) {
+					development_card_decks[i][j].removeTopCard();
+					break;
+				}
+			}
+		}
 	}
 }
---
+++ src/test/java/model/game/DevelopmentCardsOnTableTest.java
@@ -0,0 +1,88 @@
+package it.polimi.ingsw.model.game;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+import it.polimi.ingsw.model.card.DevelopmentCard;
+import it.polimi.ingsw.model.card.Deck;
+
+public class DevelopmentCardsOnTableTest {
+	DevelopmentCardsOnTable dct;
+	Deck[][] test_development_card_decks;
+
+	/**
+	 *
+	 */
+	@BeforeEach
+	public void createDevelopmentCardsOnTable() {
+		//TODO: Functional?
+		this.test_development_card_decks = new Deck[3][4];
+		for (int i = 0; i < 12; i++) {
+			ArrayList<DevelopmentCard> a = new ArrayList<DevelopmentCard>();
+			for (int j = 0; j < 4; j++) {
+				DevelopmentCard c = new DevelopmentCard(4 * i + j);
+				a.add(c);
+			}
+			test_development_card_decks[i / 4][i % 4] = new Deck(4, a, i);
+		}
+		this.dct = new DevelopmentCardsOnTable(test_development_card_decks);
+	}
+
+	/**
+	 * Check if the top cards returned are the same as the one used to create the table
+	 */
+	@Test
+	public void getTopCardsTest() {
+		DevelopmentCard[][] top_cards = dct.getTopCards();
+		assertFalse(top_cards == null);
+		for (int i = 0; i < top_cards.length; i++) {
+			for (int j = 0; j < top_cards[i].length; j++) {
+				assertEquals(top_cards[i][j], test_development_card_decks[i][j].getTopCard());
+			}
+		}
+	}
+
+	/**
+	 * Check if all the cards returned are different from each other
+	 */
+	@Test
+	public void allCardsDifferentTest() {
+		DevelopmentCard[][] top_cards = dct.getTopCards();
+		HashSet<DevelopmentCard> set = new HashSet<DevelopmentCard>();
+		for (DevelopmentCard[] cards_row : top_cards) {
+			for (DevelopmentCard card : cards_row) {
+				// if the element is already in the set set.add will return false
+				assertTrue(set.add(card));
+			}
+		}
+	}
+
+	/**
+	 * Get a card from a random Deck and check that the card got removed
+	 */
+	@RepeatedTest(value = 3)
+	public void getFromDeckTest() {
+		Random random = new Random();
+		DevelopmentCard[][] top_cards_before = dct.getTopCards();
+		int row = random.nextInt(3);
+		int column = random.nextInt(4);
+		System.out.printf("Random: %d %d\n", row, column);
+		dct.getFromDeck(top_cards_before[row][column]);
+		DevelopmentCard[][] top_cards_after = dct.getTopCards();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (i == row && j == column) {
+					assertNotEquals(top_cards_before[row][column], top_cards_after[row][column]);
+					System.out.printf("Diversa: %d %d\n", top_cards_before[row][column].getId(), top_cards_after[row][column].getId());
+				} else {
+					assertEquals(top_cards_before[i][j], top_cards_after[i][j]);
+					System.out.printf("Uguale: %d %d\n", top_cards_before[i][j].getId(), top_cards_after[i][j].getId());
+				}
+			}
+		}
+	}
+}
