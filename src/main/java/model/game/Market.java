7c88aef100e22d8bd7e0538765ba02c233f23b3f
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-03-21 19:12:28 +0100

    Finished first iteration on Market

--- src/main/java/model/game/Market.java
+++ src/main/java/model/game/Market.java
@@ -8,12 +8,14 @@ import it.polimi.ingsw.model.resources.Resource;
 /* Singleton */
 public class Market {
 	private static Market instance;
+	private final int dim_rows = 4;
+	private final int dim_cols = 3;
 	private Resource[][] market_board;
 	private Resource free_marble;
 	private ArrayList<Resource> all_resources;
 
 	private Market() {
-		this.market_board = new Resource[3][4];
+		this.market_board = new Resource[dim_cols][dim_rows];
 		this.all_resources = new ArrayList<Resource>();
 		all_resources.add(null);
 		all_resources.add(null);
@@ -31,18 +33,13 @@ public class Market {
 		randomizeMarket();
 	}
 
-	private void shiftRow(int index) {
-		return;
-	}
-
-	private void shiftColumn(int index) {
-		return;
-	}
-
+	/**
+	 * Put the resources in radom order; the last one is the free marble
+	 */
 	private void randomizeMarket() {
 		Random random = new Random();
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 4; j++) {
+		for (int i = 0; i < dim_cols; i++) {
+			for (int j = 0; j < dim_rows; j++) {
 				Resource res = all_resources.remove(random.nextInt(all_resources.size()));
 				this.market_board[i][j] = res;
 			}
@@ -57,16 +54,70 @@ public class Market {
 		return instance;
 	}
 
+	/**
+	 * Getters
+	 */
+	public Resource[][] peekMarket() {
+		return this.market_board;
+	}
+
+	public Resource getFreeMarble() {
+		return this.free_marble;
+	}
+
+	/**
+	 * Shift a column one to the left, the free_marble becomes the last element of the column
+	 *
+	 * @param index the index of the column to shift
+	 */
+	private void shiftColumn(int index) {
+		Resource old_free_marble = this.free_marble;
+		this.free_marble = this.market_board[0][index];
+		for (int i = 0; i < dim_cols - 1; i++) {
+			this.market_board[i][index] = this.market_board[i + 1][index];
+		}
+		this.market_board[dim_cols - 1][index] = old_free_marble;
+	}
+
+	/**
+	 * Shift a row one to the left, the free_marble becomes the last element of the row
+	 *
+	 * @param index the index of the row to shift
+	 */
+	private void shiftRow(int index) {
+		Resource[] new_row = new Resource[4];
+		new_row[dim_rows - 1] = this.free_marble;
+		this.free_marble = this.market_board[index][0];
+		for (int i = 0; i < dim_rows - 1; i++) {
+			new_row[i] = this.market_board[index][i + 1];
+		}
+		this.market_board[index] = new_row;
+	}
+
+	/**
+	 * Get the resources stores in a column and shift it
+	 *
+	 * @param index the index of the column to shift
+	 * @return an array of the resources taken by the player
+	 */
 	public Resource[] getColumn(int index) {
-		Resource[] column = {
-			market_board[0][index],
-			market_board[1][index],
-			market_board[2][index]
-		};
+		Resource[] column = new Resource[dim_cols];
+		for (int i = 0; i < dim_cols; i++) {
+			column[i] = market_board[i][index];
+		}
+		this.shiftColumn(index);
 		return column;
 	}
 
+	/**
+	 * Get the resources stores in a row and shift it
+	 *
+	 * @param index the index of the row to shift
+	 * @return an array of the resources taken by the player
+	 */
 	public Resource[] getRow(int index) {
-		return market_board[index];
+		Resource[] row = market_board[index];
+		this.shiftRow(index);
+		return row;
 	}
 }
--- src/test/java/model/game/MarketTest.java
+++ src/test/java/model/game/MarketTest.java
@@ -3,14 +3,131 @@ package it.polimi.ingsw.model.game;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import it.polimi.ingsw.model.resources.Resource;
 
 public class MarketTest {
 	/**
+	 * Add a resource to a HashMap while updating its index
+	 *
+	 * @param map HashMap containing the number of times a resource is found
+	 * @param r resource to insert to the HashMap
+	 */
+	private void addToMap(HashMap<Resource, Integer> map, Resource r) {
+		if (map.containsKey(r)) {
+			map.put(r, map.get(r) + 1);
+		} else {
+			map.put(r, 1);
+		}
+	}
+
+	/**
 	 * Checks if every item is in the Market, including the free marble
 	 */
 	@Test
-	@Disabled
 	public void checkEverythingTest() {
+		Market market = Market.getIstance();
+
+		HashMap<Resource, Integer> number_of_resources = new HashMap<Resource, Integer>();
+
+		Resource[][] market_board = market.peekMarket();
+		for (Resource[] row : market_board) {
+			for (Resource r : row) {
+				addToMap(number_of_resources, r);
+			}
+		}
+
+		addToMap(number_of_resources, market.getFreeMarble());
+
+		assertEquals(2, number_of_resources.get(Resource.COIN));
+		assertEquals(2, number_of_resources.get(Resource.SHIELD));
+		assertEquals(2, number_of_resources.get(Resource.SERVANT));
+		assertEquals(2, number_of_resources.get(Resource.STONE));
+		assertEquals(1, number_of_resources.get(Resource.FAITH));
+		assertEquals(4, number_of_resources.get(null));
+	}
+
+	/**
+	 * @param initial_row a row of the Market before it was shifted
+	 * @param final_row the same row of the Market after it was shifted
+	 * @return true if the shift was correct
+	 */
+	private boolean checkIfRowShifted(Resource[] initial_row, Resource[] final_row) {
+		return (initial_row[1] == final_row[0]) && (initial_row[2] == final_row[1]) && (initial_row[3] == final_row[2]);
+	}
+
+	/**
+	 * Shift all rows once and check if the shift was successful
+	 */
+	@Test
+	public void shiftAllRowsTest() {
+		Market market = Market.getIstance();
+
+		for (int i = 0; i < 3; i++) {
+			Resource[] initial_row = market.getRow(i);
+			Resource[] final_row = market.getRow(i);
+			assertTrue(checkIfRowShifted(initial_row, final_row));
+		}
+	}
+
+	/**
+	 * Shift the same random row four times and check that the free marble was positioned correctly
+	 */
+	@RepeatedTest(value = 3)
+	public void shiftRowFourTimesTest() {
+		Market market = Market.getIstance();
+		Random random = new Random();
+		int row = random.nextInt(3);
+
+		Resource[] first = market.getRow(row);
+		Resource[] second = market.getRow(row);
+		Resource[] third = market.getRow(row);
+		Resource[] fourth = market.getRow(row);
+
+		assertTrue(first[0] == third[3]);
+		assertTrue(second[0] == fourth[3]);
+		assertTrue(first[3] == second[2] && third[1] == fourth[0] && first[3] == fourth[0]);
+	}
+
+	/**
+	 * @param initial_column a column of the Market before it was shifted
+	 * @param final_column the same column of the Market after it was shifted
+	 * @return true if the shift was correct
+	 */
+	private boolean checkIfColumnShifted(Resource[] initial_column, Resource[] final_column) {
+		return (initial_column[1] == final_column[0]) && (initial_column[2] == final_column[1]);
+	}
+
+	/**
+	 * Shift all columns once and check if the shift was successful
+	 */
+	@Test
+	public void shiftAllColumnsTest() {
+		Market market = Market.getIstance();
+
+		for (int i = 0; i < 3; i++) {
+			Resource[] initial_column = market.getColumn(i);
+			Resource[] final_column = market.getColumn(i);
+			assertTrue(checkIfColumnShifted(initial_column, final_column));
+		}
+	}
+
+	/**
+	 * Shift the same random row four times and check that the free marble was positioned correctly
+	 */
+	@RepeatedTest(value = 3)
+	public void shiftColumnFourTimesTest() {
+		Market market = Market.getIstance();
+		Random random = new Random();
+		int column = random.nextInt(4);
+
+		Resource[] first = market.getColumn(column);
+		Resource[] second = market.getColumn(column);
+		Resource[] third = market.getColumn(column);
+
+		assertTrue(first[0] == third[2]);
+		assertTrue(first[2] == second[1] && third[0] == second[1]);
 	}
 }
