4cf090a87b01b0a600585905a029c72bb4dde595
Lorenzo Dall'Aglio <10608226@polimi.it>
2021-03-23 21:24:30 +0100

    First implementation of Leader abilities

--- src/main/java/model/card/WhiteMarblesAbility.java
+++ src/main/java/model/card/WhiteMarblesAbility.java
@@ -1,9 +1,22 @@
 package it.polimi.ingsw.model.card;
 
+import java.lang.IllegalArgumentException;
+
 import it.polimi.ingsw.model.card.LeaderAbility;
 
 import it.polimi.ingsw.model.resources.Resource;
 
 public class WhiteMarblesAbility implements LeaderAbility {
 	private Resource resource_type;
+	
+	public WhiteMarblesAbility (Resource bonus){
+		if (bonus != Resource.FAITH){
+			resource_type = bonus;
+		}
+		else {throw new IllegalArgumentException();}
+	}
+	
+	public Resource getResourceType(){
+		return resource_type;
+	}
 }
