b9e4d0ab527cf538ee4bfb5dcb562877e63f599a
Lorenzo Dall'Aglio <10608226@polimi.it>
2021-03-23 21:30:36 +0100

    Made Deck abstract

--- src/main/java/model/card/Deck.java
+++ src/main/java/model/card/Deck.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.model.card;
 import it.polimi.ingsw.model.card.Card;
 import java.util.Collection;
 
-public class Deck implements Collection {
+public abstract class Deck implements Collection {
 	private int max_dimension;
 
 	public Card getTopCard() {
