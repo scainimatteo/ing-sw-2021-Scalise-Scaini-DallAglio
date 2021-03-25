0417db4588c950ce09f6f9b36239b72914614260
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-03-25 14:49:44 +0100

    Modified the test for warehouse

--- src/test/java/model/player/WarehouseTest.java
+++ src/test/java/model/player/WarehouseTest.java
@@ -13,7 +13,7 @@ public class WarehouseTest {
 	 * first test for the class Warehouse
 	 */
 	@Test
-	public void firstWarehouseTest () {
+	public void firstWarehouseTest(){
 		Warehouse test_warehouse = new Warehouse();
 		int test_return = 0;
 
@@ -35,5 +35,31 @@ public class WarehouseTest {
 		assertEquals(expected_top, test_warehouse.getTopResource());
 		assertArrayEquals(expected_middle, test_warehouse.getMiddleResources());
 		assertArrayEquals(expected_bottom, test_warehouse.getBottomResources());
+	}
+
+	/**
+	 * getFromWarehouse test
+	 */
+	@Test
+	public void getFromWarehouseTest(){
+		Warehouse test_warehouse = new Warehouse();
+
+		Resource resource1 = Resource.COIN;
+		Resource resource2 = Resource.STONE;
+		Resource resource3 = Resource.SHIELD;
+		Resource resource4 = Resource.STONE;
+		Resource resource5 = Resource.STONE;
+		Resource[] test_resources1 = {resource1, resource2, resource3, resource4, resource5};
+
+		int test_return_trytoinsert = test_warehouse.tryToInsert(test_resources1);
+		assertEquals(test_return_trytoinsert, 0);
+
+		Resource[] test_return_getfromwarehouse = test_warehouse.getFromWarehouse(resource1, 1);
+		Resource[] test_resource_top = {resource1};
+		assertEquals(test_return_getfromwarehouse[0].name(), test_resource_top[0].name());
+
+		test_return_getfromwarehouse = test_warehouse.getFromWarehouse(resource3, 1);
+		Resource[] test_resource_mid = {resource3};
+		assertEquals(test_return_getfromwarehouse[0].name(), test_resource_mid[0].name());
 	}
 }
