ad506a0583c100f134f2f4968834e5d47696cbaf
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-03-30 16:20:40 +0200

    Modified the class Production and removed a test in ProductionTest

--- src/main/java/model/resources/Production.java
+++ src/main/java/model/resources/Production.java
@@ -37,9 +37,8 @@ public class Production {
 	/**
 	 * @param array_in_input is an array of Resources that must to be compared with this.required_resources
 	 * @return true if the two arrays have the same elements (even if they are not in the same position)
-	 * TODO: change public to private
 	 */
-	public boolean compareArrays(Resource[] array_in_input){
+	private boolean compareArrays(Resource[] array_in_input){
 		if(array_in_input[0].equals(this.required_resources[0]) || array_in_input[1].equals(this.required_resources[0])){
 			if(array_in_input[0].equals(this.required_resources[0])){
 				return(array_in_input[1].equals(this.required_resources[1]));
--- src/test/java/model/resources/ProductionTest.java
+++ src/test/java/model/resources/ProductionTest.java
@@ -9,29 +9,6 @@ import it.polimi.ingsw.model.resources.Production;
 import it.polimi.ingsw.model.resources.Resource;
 
 public class ProductionTest {
-	
-	/**
-	 * Test that check if the methods of the class Production are working
-	 */
-	@Test
-	public void firstProductionTest () {
-		Resource test_resource_1 = Resource.COIN;
-		Resource test_resource_2 = Resource.SHIELD;
-		Resource test_resource_3 = Resource.STONE;
-		Resource test_resource_4 = Resource.FAITH;
-
-		Resource[] required_resource = {test_resource_1, test_resource_2};
-		Resource[] produced_resource = {test_resource_3, test_resource_4};
-
-		Production test_production = new Production (required_resource, produced_resource);
-
-		Resource test_resource_5 = Resource.COIN;
-		Resource test_resource_6 = Resource.SHIELD;
-		Resource[] input_resource = {test_resource_6, test_resource_5};
-		
-		assertTrue(test_production.compareArrays(input_resource));
-	}
-
 	/**
 	 * Test for the method activateProduction
 	 */
