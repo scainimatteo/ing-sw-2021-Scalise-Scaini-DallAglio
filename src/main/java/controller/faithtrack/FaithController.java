698044b1e9b24286b5800ea8addd356c8f3e807f
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-04-12 15:29:31 +0200

    Created the class FaithController

---
+++ src/main/java/controller/faithtrack/FaithController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.controller.faithtrack;
+
+public class FaithController {
+	public final int faith_gained;
+	public final int faith_to_redistribute;
+
+	public FaithController(int gained, int to_redistribute){
+		this.faith_gained = gained;
+		this.faith_to_redistribute = to_redistribute;
+	}
+}
