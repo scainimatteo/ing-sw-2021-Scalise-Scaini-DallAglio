1efe05f1da78aa0c5c525c99df6680e532bfe27d
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-04-16 16:00:30 +0200

    Modified the class FaithController

--- src/main/java/controller/util/FaithController.java
+++ src/main/java/controller/util/FaithController.java
@@ -1,10 +1,14 @@
 package it.polimi.ingsw.controller.util;
 
+import it.polimi.ingsw.model.player.Player;
+
 public class FaithController {
+	public final Player player;
 	public final int faith_gained;
 	public final int faith_to_redistribute;
 
-	public FaithController(int gained, int to_redistribute){
+	public FaithController(Player player, int gained, int to_redistribute){
+		this.player = player;
 		this.faith_gained = gained;
 		this.faith_to_redistribute = to_redistribute;
 	}
