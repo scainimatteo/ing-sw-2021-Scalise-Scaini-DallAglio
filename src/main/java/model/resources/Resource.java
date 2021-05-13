cb954d92c41d5db8f7f9726f770a5bc27cc63f0a
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-05-13 14:54:28 +0200

    Created the method printText for the class Resources

--- src/main/java/model/resources/Resource.java
+++ src/main/java/model/resources/Resource.java
@@ -1,19 +1,33 @@
 package it.polimi.ingsw.model.resources;
 
-public enum Resource {
-	COIN ("YELLOW"),
-	STONE ("GREY"),
-	SERVANT ("PURPLE"),
-	SHIELD ("BLUE"),
-	FAITH ("RED");
+import it.polimi.ingsw.util.ANSI;
 
-	private String color;
+public enum Resource {
+	COIN ("co"),
+	STONE ("st"),
+	SERVANT ("se"),
+	SHIELD ("sh"),
+	FAITH ("fa");
+
+	private String abbreviation;
 
 	private Resource(String color){
-		this.color = color;
+		this.abbreviation = abbreviation;
 	}
 
-	public String getColor(){
-		return this.color;
+	public String getAbbreviation(){
+		return this.abbreviation;
+	}
+
+	public String printMarble(){
+		char circle = '\u25EF';
+		switch (this){
+			case COIN: return ANSI.yellow(Character.toString(circle));
+			case STONE: return ANSI.green(Character.toString(circle));
+			case SERVANT: return ANSI.magenta(Character.toString(circle));
+			case SHIELD: return ANSI.cyan(Character.toString(circle));
+			case FAITH: return ANSI.red(Character.toString(circle));
+			default: return ANSI.white(Character.toString(circle));
+		}
 	}
 }
