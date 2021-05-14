5eca47cf772c8c4592a7e8b2cc56a19e9e2946e7
scainimatteo <matteo.scaini.02031999@gmail.com>
2021-05-14 10:22:48 +0200

    created the method printText for the development card

--- src/main/java/model/card/DevelopmentCard.java
+++ src/main/java/model/card/DevelopmentCard.java
@@ -7,7 +7,14 @@ import it.polimi.ingsw.model.resources.Resource;
 import it.polimi.ingsw.model.resources.Production;
 import it.polimi.ingsw.model.resources.ProductionInterface;
 
-public class DevelopmentCard extends Card implements ProductionInterface {
+import it.polimi.ingsw.view.Viewable;
+
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class DevelopmentCard extends Card implements ProductionInterface, Viewable, Serializable {
 	private static final long serialVersionUID = 8L;
 	private Production production;
 	private Resource[] cost;
@@ -72,4 +79,78 @@ public class DevelopmentCard extends Card implements ProductionInterface {
 	public Resource[] activateProduction(){
 		return this.production.activateProduction();
 	}
+
+	public HashMap<Resource, Integer> numOfCost(){
+		HashMap<Resource, Integer> to_return = new HashMap<Resource, Integer>();
+
+		for (Resource res : cost){
+			if (to_return.containsKey(res)){
+				to_return.put(res, to_return.get(res) + 1);
+			} else {
+				to_return.put(res, 1);
+			}
+		}
+
+		return to_return;
+	}
+
+	public String printText(){
+		String top = "-------------------";
+		String mid = "|               |";
+
+		/**
+		 * level
+		 */
+		String spaces = "                 ";
+		String tmp = "|  " + String.valueOf(level.getLevel()) + "  " + level.getColor().toString();
+		String level = tmp + spaces.substring(tmp.length());
+		level += " |";
+
+		/**
+		 * cost
+		 */
+		HashMap<Resource, Integer> cost_to_print = this.numOfCost();
+		Set<Resource> cost_set = cost_to_print.keySet();
+		Resource[] cost_array = cost_set.toArray(new Resource[cost_set.size()]);
+
+		String cost_string = "| " + String.valueOf(cost_to_print.get(cost_array[0])) + cost_array[0].getAbbreviation();
+		if (cost_array.length >= 2){
+			cost_string += " " + String.valueOf(cost_to_print.get(cost_array[1])) + cost_array[1].getAbbreviation();
+		} 
+		if (cost_array.length >= 3){
+			cost_string += " " + String.valueOf(cost_to_print.get(cost_array[2])) + cost_array[2].getAbbreviation();
+		} 
+
+		/**
+		 * production
+		 */
+		String[] production_string = this.production.productionToText();
+		String prod1 = "| " + production_string[0] + " |";
+		String prod2 = "| " + production_string[1] + " |";
+		String prod3 = "| " + production_string[2] + " |";
+		String prod4 = "| " + production_string[3] + " |";
+		String prod5 = "| " + production_string[4] + " |";
+
+		String to_return = top + level + top + mid + cost_string + mid + top + mid + prod1 + prod2 +prod3 + prod4 + prod5 + mid + top;
+
+		return to_return;
+	}
 }
+
+/**
+ * -------------------	X
+ * |                 |	X
+ * -------------------	X
+ * |                 |	X
+ * | 0 sp 0 sp 0 sp  |  X
+ * |                 |	X
+ * -------------------	X
+ * |                 |	X
+ * |                 |	X
+ * |                 |	X
+ * |                 |	X
+ * |                 |	X
+ * |                 |	X
+ * |                 |	X
+ * -------------------	X
+ */
