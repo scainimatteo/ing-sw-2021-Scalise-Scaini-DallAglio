42dd2b0dc5574e70b115b47d999876d9e88bbf18
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-05-21 12:21:26 +0200

    Added comments to ClientHandler

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -8,8 +8,9 @@ import it.polimi.ingsw.controller.servermessage.ServerMessage;
 import it.polimi.ingsw.controller.message.Message;
 import it.polimi.ingsw.controller.Controller;
 
-import it.polimi.ingsw.server.Server;
+import it.polimi.ingsw.model.player.Player;
 
+import it.polimi.ingsw.server.Server;
 
 public class ClientHandler implements Runnable {
 	private Server server;
@@ -18,12 +19,17 @@ public class ClientHandler implements Runnable {
 	private ObjectOutputStream out;
 	private String nickname;
 	private Controller controller;
+	private Player player;
 
 	public ClientHandler(Server server, Socket client) throws IOException {
 		this.server = server;
 		this.client = client;
 		this.out = new ObjectOutputStream(client.getOutputStream()); 
 		this.in = new ObjectInputStream(client.getInputStream()); 
+	}
+
+	public Controller getController() {
+		return this.controller;
 	}
 
 	public String getNickname() {
@@ -34,14 +40,17 @@ public class ClientHandler implements Runnable {
 		this.nickname = nickname;
 	}
 
-	public Controller getController() {
-		return this.controller;
+	public void setPlayer(Player player) {
+		this.player = player;
 	}
 
 	public void setController(Controller controller) {
 		this.controller = controller;
 	}
 
+	/**
+	 * @param message the ServerMessage to send to the Client
+	 */
 	public void sendToClient(ServerMessage message) {
 		try {
 			while (true) {
@@ -69,11 +78,15 @@ public class ClientHandler implements Runnable {
 		}
 	}
 
+	/**
+	 * Receive messages from the Client and use the Controller to handle them
+	 */
 	public void run() {
 		while (true) {
 			// TODO: better exception handling
 			try {
 				Message message = (Message) in.readObject();
+				message.setPlayer(this.player);
 				this.controller.handleMessage(message);
 			} catch (IOException e) {
 				e.printStackTrace();
