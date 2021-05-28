f49eb45a0f1aab563f752dbdfeb6284ce475dc89
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-05-28 13:15:15 +0200

    Added SimpleModel in the JavaFX Application

--- src/main/java/view/gui/GUI.java
+++ src/main/java/view/gui/GUI.java
@@ -1,16 +1,34 @@
 package it.polimi.ingsw.view.gui;
 
+import javafx.application.Platform;
+
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import java.io.File;
+
+import java.util.ArrayList;
+
 import it.polimi.ingsw.client.Client;
 
 import it.polimi.ingsw.controller.servermessage.InitializingServerMessage;
 import it.polimi.ingsw.controller.servermessage.ErrorMessage;
 
+import it.polimi.ingsw.model.game.Turn;
+
+import it.polimi.ingsw.view.simplemodel.SimplePlayer;
+import it.polimi.ingsw.view.simplemodel.SimpleGame;
 import it.polimi.ingsw.view.gui.App;
 import it.polimi.ingsw.view.View;
 
 public class GUI extends View {
 	@Override
 	public void startView(Client client) {
+		App.setModel(this);
 		App.main(null);
 	}
 
@@ -22,5 +40,17 @@ public class GUI extends View {
 	@Override
 	public void handleInitializing(InitializingServerMessage initializing_message) {
 		return;
+	}
+
+	public SimpleGame getSimpleGame() {
+		return this.simple_game;
+	}
+
+	public ArrayList<SimplePlayer> getSimplePlayers() {
+		return this.simple_players;
+	}
+
+	public Turn getTurn() {
+		return this.turn;
 	}
 }
