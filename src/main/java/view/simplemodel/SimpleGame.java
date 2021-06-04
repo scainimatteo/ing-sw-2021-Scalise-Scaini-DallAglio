f1a12638694e739fde6d091fdaa80194501478a7
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2021-06-04 13:19:28 +0200

    Added order array in simpleGame

--- src/main/java/view/simplemodel/SimpleGame.java
+++ src/main/java/view/simplemodel/SimpleGame.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.view.simplemodel;
 
+import java.util.ArrayList;
+
 import it.polimi.ingsw.model.resources.Resource;
 
 import it.polimi.ingsw.model.card.DevelopmentCard;
@@ -8,14 +10,20 @@ import java.io.Serializable;
 
 public class SimpleGame implements Serializable{
 	private static final long serialVersionUID = 99234L;
+	private ArrayList<String> order;
 	private Resource[][] market;
 	private Resource free_marble;
 	private DevelopmentCard[][] development_cards_on_table;
 
-	public SimpleGame(Resource[][] market, Resource free_marble, DevelopmentCard[][] development_cards_on_table){
+	public SimpleGame(ArrayList<String> order, Resource[][] market, Resource free_marble, DevelopmentCard[][] development_cards_on_table){
+		this.order = order;
 		this.market = market;
 		this.free_marble = free_marble;
 		this.development_cards_on_table = development_cards_on_table;
+	}
+
+	public ArrayList<String> getOrder() {
+		return this.order;
 	}
 
 	public Resource[][] getMarket(){
